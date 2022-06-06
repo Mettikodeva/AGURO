c168bb08b8733bb66e466314251e7063970fc068
Mettikodeva-Nara <pancasila.day@gmail.com>
2022-06-06 23:47:37 +0700

    trying using protothreading

--- main/main.ino
+++ main/main.ino
@@ -1,67 +1,49 @@
 #include "aguro.h"
 #include "sensor.h"
-#include "definitions.h"
+#include "protothreads.h"
+
 Aguro aguro;
 Sensor mysensor;
-void (*resetFunc)(void) = 0;
+pt ptButton;
+
+
+int Button(struct pt* pt, Aguro* aguro){
+    PT_BEGIN(pt);
+    for(;;){
+        Serial.println("this is check button");
+        aguro->checkButton();
+        Serial.println("finish check button");
+        PT_YIELD(pt);
+    }
+    PT_END(pt);
+}
+
+pt ptLoop;
+int main_loop(struct pt* pt, Aguro* aguro){
+    PT_BEGIN(pt);
+    for(;;){
+        Serial.println("this is main loop");
+        aguro->updateSensor();
+        aguro->traceLine(200);
+        // delay(2000);
+        PT_YIELD(pt);
+    }
+    PT_END(pt);
+}
 
 void setup()
 {
-    // Serial.begin(115200);
+    Serial.begin(115200);
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
     FastLED.setBrightness(BRIGHTNESS);
     aguro.init(true, &mysensor);
+    PT_INIT(&ptButton);
+    PT_INIT(&ptLoop);
 }
 
-bool run = false;
-int pb_counter = 0;
-void reset()
-{
-    blink_led(5, 100, CRGB::Yellow);
-    aguro.stop_motor();
-    resetFunc();
-}
 
 void loop()
 {
-    if (digitalRead(PB) == HIGH)
-    {
-        run = !run;
-        pb_counter++;
-        if (pb_counter > 200)
-            reset();
-    }
-    else
-    {
-        aguro.updateSensor();
-        if (pb_counter > 100)
-        { // stop the program
-            aguro.stop();
-        }
-    }
-    aguro.stop_motor();
-    blink_led(1, 100, CRGB::White);
-    while (aguro.isStarted())
-    {
-        if (digitalRead(PB) == HIGH)
-        {
-            run = !run;
-            pb_counter++;
-            if (pb_counter > 200)
-                reset();
-        }
-        else
-        {
-            if (pb_counter > 100)
-            { // stop the program
-              // aguro.stop();
-            }
-        }
-        if (run)
-        {
-            aguro.traceLine(180);
-        }
-        else
-            aguro.updateSensor();
-    }
+    PT_SCHEDULE(Button(&ptButton, &aguro));
+    PT_SCHEDULE(main_loop(&ptLoop, &aguro));
 }
