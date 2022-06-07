6b545604181139a151e3cf1a93757d7a0f2144e5
Mettikodeva-Nara <pancasila.day@gmail.com>
2022-06-08 02:18:06 +0700

    remove protothread, add static function helper to attach to button function

--- main/main.ino
+++ main/main.ino
@@ -1,49 +1,36 @@
 #include "aguro.h"
-#include "sensor.h"
-#include "protothreads.h"
+// #include "protothreads.h"
 
 Aguro aguro;
 Sensor mysensor;
-pt ptButton;
 
-
-int Button(struct pt* pt, Aguro* aguro){
-    PT_BEGIN(pt);
-    for(;;){
-        Serial.println("this is check button");
-        aguro->checkButton();
-        Serial.println("finish check button");
-        PT_YIELD(pt);
+static void shortPress()
+{
+    if (aguro.isStarted())
+    {
+        aguro.stop();
     }
-    PT_END(pt);
+    else
+    {
+        aguro._start();
+    }
 }
-
-pt ptLoop;
-int main_loop(struct pt* pt, Aguro* aguro){
-    PT_BEGIN(pt);
-    for(;;){
-        Serial.println("this is main loop");
-        aguro->updateSensor();
-        aguro->traceLine(200);
-        // delay(2000);
-        PT_YIELD(pt);
-    }
-    PT_END(pt);
+static void longPress()
+{
+    mysensor.calibrateLine();
 }
 
 void setup()
 {
     Serial.begin(115200);
+    myPB.attachLongPress(longPress);
+    myPB.attachShortPress(shortPress);
+    myPB.start();
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
     FastLED.setBrightness(BRIGHTNESS);
     aguro.init(true, &mysensor);
-    PT_INIT(&ptButton);
-    PT_INIT(&ptLoop);
 }
-
 
 void loop()
 {
-    PT_SCHEDULE(Button(&ptButton, &aguro));
-    PT_SCHEDULE(main_loop(&ptLoop, &aguro));
 }
