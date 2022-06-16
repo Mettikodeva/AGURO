11ac5a75fc68506f8990eae41f573fc2c2c99f27
Mettikodeva-Nara <pancasila.day@gmail.com>
2022-06-16 10:12:54 +0700

    nothing important

--- main/main.ino
+++ main/main.ino
@@ -6,30 +6,32 @@ Aguro aguro;
 Sensor mysensor;
 
 bool start_state = true;
-void myInterrupt(){
+void myInterrupt()
+{
     start_state = !start_state;
-    while(start_state);
+    while (start_state)
+        ;
 }
-
 
 void setup()
 {
     // Serial.begin(115200);
     pinMode(3, INPUT_PULLUP);
     delayMicroseconds(100);
-    attachInterrupt(digitalPinToInterrupt(3),myInterrupt,FALLING);
+    attachInterrupt(digitalPinToInterrupt(3), myInterrupt, FALLING);
+
+    // LED
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
     FastLED.setBrightness(BRIGHTNESS);
-    Serial.println("Setup");
+
+    // AGURO
     aguro.init(true, &mysensor);
-    Serial.println("Setup finish");
     aguro._start();
     aguro.magnet(false);
 }
 
 void loop()
 {
-    // aguro.traceLine(100);
 
     int speed = 100;
     // aguro.followUntil(TJ, speed);
@@ -48,15 +50,8 @@ void loop()
     // // delay(200);
     // delay(200);
     // aguro.magnet(false);
-    aguro.mundur(90,2200);
+    aguro.mundur(90, 2200);
     delay(1000);
-    // delay(200);
-    // aguro.followUntil(FL, speed);
-
-    // delay(5000);
-    // aguro.right(150,360); // ~90 degree
-    // delay(2000);
-    // delay(5000);
     // aguro.followUntil(FR, speed);
     // delay(5000);
     // aguro.followUntil(FL, speed);
@@ -64,14 +59,4 @@ void loop()
     // aguro.followUntil(FL, speed);
     // aguro.left(155,340);
     // delay(2000);
-
-    // delay(1000);
-    // delay(10000);
-
-    // aguro.followUntil(FL, 115);
-    // aguro.followUntil(FL, 115);
-    // aguro.followUntil(FL, 115);
-    // for (int i = 0; i < 3; i++)
-    //     aguro.followUntil(TJ, 120);
-    // aguro.followUntil(FL, 120);
 }
