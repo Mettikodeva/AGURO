7cdac14bc65b79b8745f12bf44409bf4deafc2de
Mettikodeva-Nara <pancasila.day@gmail.com>
2022-06-22 13:31:58 +0700

    same as before, but this commit only sensor calibrating algorithm on kalibrasi program

--- kalibrasi/sensor.cpp
+++ kalibrasi/sensor.cpp
@@ -2,32 +2,21 @@
 
 void Sensor::init(bool debug)
 {
+    pinMode(PushButton, INPUT_PULLUP);
     pinMode(Trig, OUTPUT);
     pinMode(Echo, INPUT);
-    pinMode(sp1, INPUT);
-    pinMode(sp2, INPUT);
-    pinMode(sp3, INPUT);
-    pinMode(sp4, INPUT);
-    pinMode(sp5, INPUT);
-    pinMode(sp6, INPUT);
-    pinMode(sp7, INPUT);
-    pinMode(sp8, INPUT);
+    pinMode(MUXData, INPUT);
+    pinMode(MUXPinA, OUTPUT);
+    pinMode(MUXPinB, OUTPUT);
+    pinMode(MUXPinC, OUTPUT);
     this->DEBUG = debug;
 }
 
-char Sensor::readsensorline()
+void Sensor::setMux(int index)
 {
-    // noInterrupts();
-    sensors[0] = analogRead(sp1);
-    sensors[1] = analogRead(sp2);
-    sensors[2] = analogRead(sp3);
-    sensors[3] = analogRead(sp4);
-    sensors[4] = analogRead(sp5);
-    sensors[5] = analogRead(sp6);
-    sensors[6] = analogRead(sp7);
-    sensors[7] = analogRead(sp8);
-    // Serial.println("reading sensor");
-    // interrupts();
+    digitalWrite(MUXPinA, MUX[index][0]);
+    digitalWrite(MUXPinB, MUX[index][1]);
+    digitalWrite(MUXPinC, MUX[index][2]);
 }
 
 void Sensor::calibrateLine()
@@ -47,50 +36,69 @@ void Sensor::calibrateLine()
                 leds[i] = CRGB(0, 0, 0);
                 FastLED.show();
             }
-            // delay(3);
-            // FastLED.show();
         }
         shift++;
         if (shift >= NUM_LEDS + 2)
             shift = 0;
     }
-    // noInterrupts();
     digitalWrite(LED_BUILTIN, HIGH);
     // read line first, then read the background
-    int line_[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    int background_[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    int counter = 0;
-    blink_led(3,100, CRGB::Red);
-    uint32_t start = millis();
-    while (digitalRead(3))
+    int line_[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int background_[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    // int counter = 0;
+    // blink_led(3, 200, CRGB::Red);
+    Serial.println("calibrating");
+    Serial.println("line");
+    while (digitalRead(PushButton))
     {
-        this->readsensorline();
         for (int i = 0; i < 8; i++)
         {
-            line_[i] = sensors[i];
-            Serial.println("baca line " + String(sensors[i]));
+            setMux(i);
+            delayMicroseconds(10);
+            line_[i] = analogRead(MUXData);
+            Serial.print(line_[i]);
+            Serial.print(" ");
         }
+        line_[8] = analogRead(SpinFL);
+        Serial.print(line_[8]);
+            Serial.print(" ");
+        line_[9] = analogRead(SpinFR);
+        Serial.print(line_[9]);
+            Serial.print(" ");
+        Serial.println();
         delay(10);
     }
-    blink_led(3, 150, CRGB::Blue);
-    start = millis();
-    blink_led(3, 50, CRGB::Red);
-    while (digitalRead(3))
+
+    Serial.println("background");
+
+    blink_led(3,150,CRGB::Blue);
+
+    Serial.println("background");
+    while (digitalRead(PushButton))
     {
-        this->readsensorline();
         for (int i = 0; i < 8; i++)
         {
-            background_[i] = sensors[i];
+            setMux(i);
+            delayMicroseconds(10);
+            background_[i] = analogRead(MUXData);
+            Serial.print(background_[i]);
+            Serial.print(" ");
         }
+        background_[8] = analogRead(SpinFL);
+        Serial.print(background_[8]);
+            Serial.print(" ");;
+        background_[9] = analogRead(SpinFR);
+        Serial.print(background_[9]);
+            Serial.print(" ");
+        Serial.println();
         delay(10);
     }
-    blink_led(3, 150, CRGB::Blue);
     this->calibrated = true;
     if (line_[0] < background_[0])
         line_high = false;
     else
         line_high = true;
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 10; i++)
     {
         if (line_high)
         {
@@ -104,6 +112,68 @@ void Sensor::calibrateLine()
         }
     }
     // save_calibration();
+    blink_led(3,150,CRGB::Red);
+    while (digitalRead(PushButton))
+    {
+        line_[10] = analogRead(SpinBL);
+        Serial.print(line_[10]);
+            Serial.print(" ");
+        line_[11] = analogRead(SpinBR);
+        Serial.print(line_[11]);
+            Serial.print(" ");
+        Serial.println();
+        delay(10);
+    }
+    blink_led(3,150,CRGB::Red);
+    while (digitalRead(PushButton))
+    {
+        background_[10] = analogRead(SpinBL);
+        Serial.print(background_[10]);
+            Serial.print(" ");
+        background_[11] = analogRead(SpinBR);
+        Serial.print(background_[11]);
+            Serial.print(" ");
+        Serial.println();
+        delay(10);
+    }
+    blink_led(5,150,CRGB::Red);
+    this->calibrated = true;
+
+    for (int i = 10; i < 12; i++)
+    {
+        if (line_high)
+        {
+            max_line[i] = line_[i];
+            min_line[i] = background_[i];
+        }
+        else
+        {
+            max_line[i] = background_[i];
+            min_line[i] = line_[i];
+        }
+    }
+    save_calibration();
+    shift = 0;
+    for (int j = 0; j < 30; j++)
+    {
+        for (int i = 0; i < NUM_LEDS; i++)
+        {
+            if (i <= shift && i > shift - 4)
+            {
+                leds[i] = CRGB(25, 255, 0);
+                FastLED.show();
+            }
+            else
+            {
+                leds[i] = CRGB(0, 0, 0);
+                FastLED.show();
+            }
+        }
+        shift++;
+        if (shift >= NUM_LEDS + 2)
+            shift = 0;
+    }
+
     digitalWrite(LED_BUILTIN, LOW);
     for (int i = 0; i < 8; i++)
     {
@@ -111,24 +181,23 @@ void Sensor::calibrateLine()
         FastLED.show();
         delay(100);
     }
-    // interrupts();
 }
 
 void Sensor::save_calibration()
 {
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 12; i++)
     {
         EEPROMWriteInt(i * sizeof(int), max_line[i]);
-        EEPROMWriteInt(i * sizeof(int) + 8 * sizeof(int), min_line[i]);
+        EEPROMWriteInt(i * sizeof(int) + 12 * sizeof(int), min_line[i]);
     }
 }
 
 void Sensor::read_calibration()
 {
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < 12; i++)
     {
         max_line[i] = EEPROMReadInt(i * sizeof(int));
-        min_line[i] = EEPROMReadInt(i * sizeof(int) + 8 * sizeof(int));
+        min_line[i] = EEPROMReadInt(i * sizeof(int) + 12 * sizeof(int));
     }
 }
 
@@ -136,41 +205,49 @@ bool Sensor::is_line_high()
 {
     return this->line_high;
 }
-bool Sensor::readlinebool(int index)
-{
-    // IF THE SENSOR DETECT LINE THE VALUE WILL BE 1
-    // ELSE 0
-    readsensorline();
-    if (this->calibrated)
-    {
-        if (sensors[index] > (max_line[index] - min_line[index]) / 2 + min_line[index])
-            if (line_high)
-                return 1;
-            else
-                return 0;
-        else if (line_high)
-            return 0;
-        else
-            return 1;
-    }
-    else
-    {
-        if (sensors[index] > 800)
-            return 1;
-        else
-            return 0;
-    }
-}
+// bool Sensor::readlinebool(int index)
+// {
+//     // IF THE SENSOR DETECT LINE THE VALUE WILL BE 1
+//     // ELSE 0
+//     noInterrupts();
+//     setMux(index);
+//     delayMicroseconds(10);
+//     sensors[index] = analogRead(MUXData);
+//     if (this->calibrated)
+//     {
+//         if (sensors[index] > (max_line[index] - min_line[index]) / 2 + min_line[index])
+//             if (line_high)
+//                 return 1;
+//             else
+//                 return 0;
+//         else if (line_high)
+//             return 0;
+//         else
+//             return 1;
+//     }
+//     else
+//     {
+//         if (sensors[index] > 750)
+//             if (line_high)
+//                 return 1;
+//             else
+//                 return 0;
+//         else if (line_high)
+//             return 0;
+//         else
+//             return 1;
+//     }
+// }
 
 // read ultrasonic and return in cm
-float Sensor::read_ultrasonic()
-{
-    digitalWrite(Trig, LOW);
-    delayMicroseconds(2);
-    digitalWrite(Trig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(Trig, LOW);
-    float duration = pulseIn(Echo, HIGH);
-    float distance = duration / (29.1 * 2);
-    return distance;
-}
+// float Sensor::read_ultrasonic()
+// {
+//     digitalWrite(Trig, LOW);
+//     delayMicroseconds(2);
+//     digitalWrite(Trig, HIGH);
+//     delayMicroseconds(10);
+//     digitalWrite(Trig, LOW);
+//     float duration = pulseIn(Echo, HIGH);
+//     float distance = duration / (29.1 * 2);
+//     return distance;
+// }
--- kalibrasi/sensor.h
+++ kalibrasi/sensor.h
@@ -4,19 +4,41 @@
 
 // sensors pin without mux or i2c  I/O expander
 
-#define sp1 A0
-#define sp2 A1
-#define sp3 A2
-#define sp4 A3
-#define sp5 A4
-#define sp6 A5
-#define sp7 A6
-#define sp8 A7
+// #define sp1 A0
+// #define sp2 A1
+// #define sp3 A2
+// #define sp4 A3
+// #define sp5 A4
+// #define sp6 A5
+// #define sp7 A6
+// #define sp8 A7
 #define PushButton 3
-
+#define MUXData A0
+#define MUXPinA A1
+#define MUXPinB A2
+#define MUXPinC A3
+#define SpinFL A7
+#define SpinFR A6
+#define SpinBL A5
+#define SpinBR A4
+#define SFL 8
+#define SFR 9
+#define SBL 10
+#define SBR 11
 // ultrasonic pin
 #define Trig 5 // PD5 //output
 #define Echo 4 // PD4 //input
+static unsigned char MUX[8][3] = {
+    {0, 0, 0},
+    {1, 0, 0},
+    {0, 1, 0},
+    {1, 1, 0},
+    {0, 0, 1},
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 1}};
+    
+
 
 class Sensor
 {
@@ -27,21 +49,21 @@ class Sensor
     */
 private:
     bool DEBUG;
-    int sensors[8];
+    int sensors[12];
     // to save the calibration
-    int max_line[8];
-    int min_line[8];
+    int max_line[12];
+    int min_line[12];
     bool calibrated = false;
     bool line_high = false;
     void save_calibration();
     void read_calibration();
+    void setMux(int);
 
 public:
     bool is_line_high();
     void init(bool debug = true);
     // to calibrate the line sensor for readlinebool
     void calibrateLine();
-    char readsensorline();
     // to read the line sensor, SENSOR SHOULD BE CALIBRATED!
     //  1 = LINE FOUND
     //  0 = LINE NOT FOUND
